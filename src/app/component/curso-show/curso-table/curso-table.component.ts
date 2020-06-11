553636b18a3b0e476de166c992692a8afcd0e49e
ivancas84 <ivancas84@gmail.com>
2020-06-11 09:03:54 -0300

    Adaptar componente y componentes anidados de curso-show a la version 1 de angular-core

--- src/app/component/curso-show/curso-table/curso-table.component.ts
+++ src/app/component/curso-show/curso-table/curso-table.component.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
 import { ShowElementComponent } from '@component/show-element/show-element.component';
-import { DataDefinitionService } from '@service/data-definition/data-definition.service';
-import { isEmptyObject } from '@function/is-empty-object.function';
-import { forkJoin } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-curso-table',
   templateUrl: './curso-table.component.html',
 })
-export class CursoTableComponent extends ShowElementComponent {
-
-  readonly entityName = 'curso';
-
-  constructor(protected dd: DataDefinitionService) {
-    super();
+export class CursoTableComponent extends ShowElementComponent { 
+  
+  constructor(protected router: Router) { 
+    super(router);
   }
 
 }
