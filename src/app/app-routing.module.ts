81d7b10352ed28c7f1acaf055a854746aa9435bb
ivancas84 <ivancas84@gmail.com>
2020-08-20 00:51:21 -0300

    Incorporar archivos estructurales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import { AfiliacionAdminComponent } from '@component/afiliacion-admin/afiliacion-admin/afiliacion-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: 'afiliacion-admin', component: AfiliacionAdminComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
