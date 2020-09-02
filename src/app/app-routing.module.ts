c76acacfe75265e5812f864aea87325f902955b3
ivancas84 <ivancas84@gmail.com>
2020-09-02 10:26:25 -0300

    Desarrollo de componente comision-show

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PersonaShowComponent } from '@component/persona-show/persona-show/persona-show.component';
+import { ComisionShowComponent } from '@component/comision-show/comision-show/comision-show.component';
 
 const routes: Routes = [
   { path: 'persona-show', component: PersonaShowComponent, pathMatch: 'full' },
+  { path: 'comision-show', component: ComisionShowComponent, pathMatch: 'full' },
+
 ];
 
 @NgModule({
