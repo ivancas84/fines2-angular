cac813664584df3a457c078cc77739eb1e7b6b2e
ivancas84 <ivancas84@gmail.com>
2020-09-16 10:14:41 -0300

    Actualizar librerias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MaterialFileInputModule } from 'ngx-material-file-input';
+//import { ClipboardModule } from '@angular/cdk/clipboard';
+//import { DATE_FORMATS } from './core/const/DATE_FORMATS';
+
 import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher, MAT_DATE_LOCALE, DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatNativeDateModule } from '@angular/material/core';
+//import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
 
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
@@ -30,15 +35,11 @@ import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
-import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
-
-import { MaterialFileInputModule } from 'ngx-material-file-input';
 
 import { DataDefinitionService } from '@service/data-definition/data-definition.service';
 import { SessionStorageService } from '@service/storage/session-storage.service';
 import { ParserService } from '@service/parser/parser.service';
 import { ValidatorsService } from '@service/validators/validators.service';
-
 
 import { LabelPipe } from '@pipe/label.pipe';
 import { ToDatePipe } from '@pipe/to-date.pipe';
@@ -51,8 +52,12 @@ import { DataDefinitionLoaderService } from '@service/data-definition-loader.ser
 
 import { DialogAlertComponent } from '@component/dialog-alert/dialog-alert.component';
 import { InputAutocompleteComponent } from '@component/input-autocomplete/input-autocomplete.component';
+import { InputDateComponent } from '@component/input-date/input-date.component';
 import { InputSelectComponent } from '@component/input-select/input-select.component';
 import { InputSearchGoComponent } from '@component/input-search-go/input-search-go.component';
+import { InputTextComponent } from '@component/input-text/input-text.component';
+//import { InputYmComponent } from '@component/input-ym/input-ym.component';
+import { InputYearComponent } from '@component/input-year/input-year.component';
 import { MenuComponent } from '@component/menu/menu.component';
 import { SearchAllComponent } from '@component/search-all/search-all.component';
 import { LabelComponent } from '@component/label/label.component';
@@ -88,12 +93,21 @@ export const APP_DATE_FORMATS = {
   declarations: [
     AppComponent,
 
-    LabelPipe, ToDatePipe, ToTimePipe, SiNoPipe, SummaryPipe, StoragePipe,
+    LabelPipe, 
+    ToDatePipe, 
+    ToTimePipe, 
+    SiNoPipe, 
+    SummaryPipe, 
+    StoragePipe,
 
     DialogAlertComponent,
     InputAutocompleteComponent,
+    InputDateComponent,
     InputSelectComponent,
     InputSearchGoComponent,
+    InputTextComponent,
+  	InputYearComponent,
+    //InputYmComponent,
     MenuComponent,
     SearchAllComponent,
     LabelComponent,
@@ -108,10 +122,13 @@ export const APP_DATE_FORMATS = {
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
     FlexLayoutModule,
+
+    //ClipboardModule,
 
     MatAutocompleteModule,
     MatButtonModule,
@@ -139,9 +156,9 @@ export const APP_DATE_FORMATS = {
     {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher},
     {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2000, verticalPosition:"top", horizontalPosition:"right"}},
     {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
-    {provide: MAT_DATE_LOCALE, useValue: 'es-AR' },
-    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]},
-    {provide: MAT_DATE_FORMATS, useValue: APP_DATE_FORMATS},
+    //{provide: MAT_DATE_LOCALE, useValue: 'es-AR' },
+    //{provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]},
+    //{provide: MAT_DATE_FORMATS, useValue: DATE_FORMATS},
 
     DataDefinitionService, 
     SessionStorageService, 
