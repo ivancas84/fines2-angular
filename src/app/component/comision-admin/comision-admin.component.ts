15e4df7da27ba3e97b083f1544c660ee30e42a4c
ivancas84 <ivancas84@gmail.com>
2022-01-05 09:33:47 -0300

    Incorporar boton para crear cursos

--- src/app/component/comision-admin/comision-admin.component.ts
+++ src/app/component/comision-admin/comision-admin.component.ts
@@ -2,12 +2,21 @@ import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FieldWidthOptions } from '@class/field-width-options';
 import { ConfigFormGroupFactory, FormControlConfig, FormStructureConfig } from '@class/reactive-form-config';
+import { AbstractControlViewOption } from '@component/abstract-control-view/abstract-control-view.component';
 import { AdminComponent } from '@component/admin/admin.component';
+import { ControlValueConfig } from '@component/control-value/control-value.component';
+import { DialogAlertComponent } from '@component/dialog-alert/dialog-alert.component';
+import { EventButtonConfig } from '@component/event-button/event-button.component';
+import { EventIconConfig } from '@component/event-icon/event-icon.component';
 import { FieldsetDynamicConfig } from '@component/fieldset/fieldset-dynamic.component';
 import { InputAutocompleteConfig } from '@component/input-autocomplete/input-autocomplete.component';
 import { InputCheckboxConfig } from '@component/input-checkbox/input-checkbox.component';
 import { InputSelectConfig } from '@component/input-select/input-select.component';
 import { InputTextConfig } from '@component/input-text/input-text.component';
+import { TableDynamicConfig } from '@component/table/table-dynamic.component';
+import { TextareaConfig } from '@component/textarea/textarea.component';
+import { Observable } from 'rxjs/internal/Observable';
+import { map, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-comision-admin',
@@ -16,11 +25,45 @@ import { InputTextConfig } from '@component/input-text/input-text.component';
 export class ComisionAdminComponent extends AdminComponent {
   readonly entityName: string = "comision"; 
   
-  form: FormGroup = this.fb.group({})
+  queryData(): Observable<any> {
+    /**
+     * Consulta y definicion de datos
+     * Se utilizan dos servicios para definir datos, 
+     * cada servicio identifica las relaciones correspondientes de la configuracion (fk o um) 
+     * y efectua las acciones correspondientes para obtener y asociar datos
+     */
+    return this.relFk.uniqueConfig(this.entityName, this.display$.value, this.config.controls).pipe(
+      switchMap(
+        row => {
+          return this.relUm.group(this.entityName, row, this.config.controls)
+        }
+      ),
+      switchMap(
+        row => {
+          return this.dd.postAllConnection(
+            row["curso/comision"], 
+            "info",
+            "id",
+            "curso",
+            "curso_horario",
+            {"horario":"horario"}
+            ).pipe(
+              map(
+                () => { return row }
+              )
+            )
+        }
+      ),
+      tap(
+        row =>{ console.log(row)}
+      )
+    )
+    //return this.dd.unique(this.entityName, this.display$.value) 
+    //return this.dd.post(this.queryApi, this.entityName, this.display$.value);
+  }
   
   config: FormStructureConfig = new FormStructureConfig({
     title:"Comision"
-
   },{
     "comision": new FieldsetDynamicConfig(
       {
@@ -72,21 +115,62 @@ export class ComisionAdminComponent extends AdminComponent {
           label:"Publicada",
           width:new FieldWidthOptions
         }),
-        "observaciones": new InputCheckboxConfig({
+        "observaciones": new TextareaConfig({
           label:"Observaciones",
           width:new FieldWidthOptions
         }),
       }
+    ),
+    "curso/comision": new TableDynamicConfig({title:"Cursos"}, {
+        "asignatura": new  InputSelectConfig(
+          {entityName:"asignatura"}
+        ),
+        "horario": new  ControlValueConfig(
+          {readonly:true, disabled:true}
+        ),
+      }
     )
   })
 
-  
-  
-  ngOnInit() {
-    var fgc = new ConfigFormGroupFactory(this.config.controls["comision"])
-    this.form.addControl("comision", fgc.formGroup())
-
-    super.ngOnInit()
+  ngOnInit(){
+    super.ngOnInit();
+    this.optFooter.push({
+      config: new EventIconConfig({
+        icon: "list", //texto del boton
+        action: "crear_cursos_comision", //accion del evento a realizar
+        color: "primary",
+        title:"Crear Cursos Comision",
+        fieldEvent: this.optField
+      })
+    })
   }
+
+  switchOptField(data: any){
+    switch(data.action){
+      case "crear_cursos_comision": 
+        this.crearCursosComision()
+        break;
+      default: return super.switchOptField(data)
+    }
+  }
+
+
+  crearCursosComision(){
+    var s = this.dd.post("persist","crear_cursos_comision",this.form.controls["comision"].get("id").value)
+    .subscribe(
+      response => {
+        this.response = response
+        this.submitted()        
+      },
+      error => { 
+        this.dialog.open(DialogAlertComponent, {
+          data: {title: "Error", message: error.error}
+        });
+        this.isSubmitted = false;
+      }
+    );
+    this.subscriptions.add(s);
+  }
+  
 }
 
