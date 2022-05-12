b5a7d8699c34d1a75190218c96b6571e2e0b741c
ivancas84 <ivancas84@gmail.com>
2022-05-12 13:59:34 -0300

    Agregar pipe en desarrollo: desaprobado.

--- src/app/pipe/desaprobado.pipe.ts
+++ src/app/pipe/desaprobado.pipe.ts
@@ -4,7 +4,7 @@ import { FormGroup, FormControl } from "@angular/forms";
 @Pipe({
     name: 'aprobado',
 })
-export class AprobadoPipe implements PipeTransform {
+export class DesaprobadoPipe implements PipeTransform {
 transform(control: FormGroup): any {
     return (((control.get("nota_final") as FormControl).value >= 7) || ((control.get("crec") as FormControl).value >= 4));
 }
