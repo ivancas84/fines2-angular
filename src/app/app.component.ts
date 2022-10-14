10aac80c127cda9fa571a7ee9eef5ce8710e13cc
ivancas84 <ivancas84@gmail.com>
2022-10-14 07:18:45 -0300

    Incorporar inicializacion de datos de sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataDefinitionService } from '@service/data-definition/data-definition.service';
+import { LocalStorageService } from '@service/storage/local-storage.service';
+import { SessionStorageService } from '@service/storage/session-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fines2-angular';
+export class AppComponent implements OnInit{
+  constructor(
+    protected session: SessionStorageService, 
+    protected local: LocalStorageService, 
+    protected dd: DataDefinitionService, 
+
+  ) { }
+
+  ngOnInit(): void {
+    this.local.clear();
+    
+  }
+
+  title = 'Fines 2';
 }
